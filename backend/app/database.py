cf49cb40ecb8c7a519f8e408c48d35f4a62b1086
Minh Kha <khatruong@Khas-MacBook-Air-2.local>
2025-06-29 00:20:09 +1000

    Dev: Configuration for backend

---
+++ backend/app/database.py
@@ -0,0 +1,27 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from .config import settings
+
+from .models.users_model import User
+
+# -------------------- Module level variable --------------------->
+
+client: AsyncIOMotorClient | None = None
+
+# ----------------------- Init database -------------------------->
+
+async def init_db() -> None:
+    global client
+    client = AsyncIOMotorClient(settings.MONGODB_URI)
+    await init_beanie(
+        database=client[settings.MONGODB_DB],
+        document_models=[
+            User
+        ],
+    )
+
+# ----------------------- Close database -------------------->
+
+async def close_db() -> None:
+    if client:
+        client.close()
