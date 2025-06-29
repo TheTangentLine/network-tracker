dab8e4a3764d0b3e6dc073bd38ef08e0e7633e44
Minh Kha <khatruong@Khas-MacBook-Air-2.local>
2025-06-29 15:00:03 +1000

    Dev: Initial structure for Class User

--- backend/app/routers/users_router.py
+++ backend/app/routers/users_router.py
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post("/register")
 async def register_user(input: UserRegister):
     try:
-        register(input)
+        await register(input)
         return {"message": "User registered successfully"}
     except:
         raise HTTPException(status_code=400, detail="Failed to register user") 
@@ -24,7 +24,16 @@ async def register_user(input: UserRegister):
 @router.post("/login")
 async def login_user(input: UserLogin):
     try:
-        user_data = login(input)
+        user_data = await login(input)
         return user_data
     except:
         raise HTTPException(status_code=400, detail="Failed to login")
+    
+# --------------------- Get current user ------------------>
+
+@router.get("/me")
+async def get_current_user():
+    try:
+        user_data = get_current()
+    except:
+        raise HTTPException(status_code=400, detail="Failed to get user data")
