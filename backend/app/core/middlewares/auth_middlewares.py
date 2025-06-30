534ec0e9d5e4f880a5b99a582937e413a5f51fa7
Minh Kha <khatruong@Khas-MacBook-Air-2.local>
2025-06-30 13:54:57 +1000

    Dev: Add middleware and handling unprotected route for authentication

---
+++ backend/app/core/middlewares/auth_middlewares.py
@@ -0,0 +1,43 @@
+from fastapi import Request, Response, HTTPException
+
+from app.core.security.jwt import verify_access_token, refresh_access_token
+
+UNPROTECTED_PATHS = [
+    "/auth/login",
+    "/auth/register",
+    "/openapi.json",
+    "/docs",
+    "/docs/oauth2-redirect",
+    "/redoc",
+]
+
+async def auth_middleware(request: Request, call_next):
+
+    if request.url.path in UNPROTECTED_PATHS:
+        return await call_next(request)
+    
+    access_token = request.cookies.get("access_token")
+    refresh_token = request.cookies.get("refresh_token")
+
+    response = Response("Unauthorized", status_code=401)
+
+    if not access_token or not verify_access_token(access_token):
+        if not refresh_token:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        
+        refreshed = refresh_access_token(refresh_token)
+        access_token = refreshed["access_token"]
+        expire_time = refreshed["exp"]
+        response = await call_next(request)
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            expires=expire_time,
+            httponly=True,
+            secure=True,
+            samesite="lax"
+        )
+        return response
+
+    response = await call_next(request)
+    return response
