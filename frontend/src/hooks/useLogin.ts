f873cc65948818be85ad3a21b1b98834c8288dc6
Minh Kha <khatruong@Khas-MacBook-Air-2.local>
2025-07-06 00:04:27 +1000

    Dev: Add custom hooks for handling API Call

---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
+
+import type { UserLogin } from '../entities/User';
+
+import { login } from '../services/authService';
+
+
+export default function useLogin() {
+    const { user, setUser } = useAuth();
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate()
+
+    const handleLogin = async (input: UserLogin) => {
+        setLoading(true);
+
+        login(input)
+            .then(u => {
+                setUser({ ...user, username: u.data.username, email: u.data.email });
+                navigate('/testing');
+            })
+            .catch(e => {
+                setError(e.response.data.detail || 'An error occurred during login');
+                return null;
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+
+        return user;
+    };
+
+    return { login: handleLogin, error, loading };
+}
---
+++ frontend/src/hooks/useRegister.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { register } from '../services/authService';
+import type { UserRegister } from '../entities/User';
+
+export default function useRegister() {
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+
+    const handleRegister = async (userData: UserRegister) => {
+        setLoading(true);
+        register(userData)
+            .catch(e => {
+                setError(e.response.data.detail || 'An error occurred during registration');
+                return null;
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+    return { handleRegister, error, loading };
+}
