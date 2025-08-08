b4d2d44881ce1e3a6d56b523aa5aa96edb2cc2b7
Marvin Truong <khatruong06@gmail.com>
2025-08-08 23:23:16 +1000

    feat: handle loading page

--- frontend/src/pages/auth/AuthDirect.tsx
+++ frontend/src/pages/auth/AuthDirect.tsx
@@ -1,10 +1,13 @@
 import { Navigate } from "react-router-dom";
 import useFetch from "../../hooks/auth/useFetch";
+import LoadingPage from "./LoadingPage";
 
 const AuthDirect = () => {
     const { user, loading } = useFetch();
 
-    if (loading) return <div>Loading...</div>;
+    if (loading) {
+        return <LoadingPage />;
+    }
     if (user && user.username) {
         return <Navigate to="/testing" replace />;
     }
