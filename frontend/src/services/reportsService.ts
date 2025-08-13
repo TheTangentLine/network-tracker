f34255b526c0fac0811289866f3a238a6a50ef5c
Marvin Truong <khatruong06@gmail.com>
2025-08-13 22:31:39 +1000

    feat: fixing API method in frontend for deleting reports

--- frontend/src/services/reportsService.ts
+++ frontend/src/services/reportsService.ts
@@ -3,18 +3,26 @@ import { generateUrlQuery } from "../utils/urlQuery";
 import type { SpeedTestReport, SpeedTestResult } from "../entities/Network";
 import type { Filter } from "../entities/Filter"
 
+// ------------------------------ Save report ------------------------------------->
+
 export async function saveReport(data: SpeedTestReport) {
     return apiClient.post('/reports/create', data);
 }
+
+// ------------------------------ Read reports ------------------------------------>
 
 export async function readReport(username: string | undefined, page: number, filter: Filter | undefined, searchText: string) {
     const baseString = generateUrlQuery(username, page, filter, searchText);
     return apiClient.get(baseString);
 }
 
+// ------------------------------ Delete reports ----------------------------------->
+
 export async function deleteReport(id: string) {
-    return apiClient.post('/reports/delete', { id });
+    return apiClient.delete(`/reports/delete/${id}`);
 }
+
+// ------------------------------ Generate PDF file -------------------------------->
 
 export async function generatePdfFile(input: SpeedTestResult) {
     return apiClient.post('/reports/genpdf', input);
